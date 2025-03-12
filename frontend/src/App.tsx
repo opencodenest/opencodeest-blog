510a97a1baddd8f3b7b16198048d1eeb064268a8
MONSTER-HARSH <harshithvs368@gmail.com>
2025-03-12 22:53:30 +0530

    force redirect to /blog, in frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,24 +3,25 @@ import NavBar from './components/nav-bar/nav-bar'
 import Footer from './components/footer/footer'
 import BlogListContainer from './components/blog-list-container/blog-list-container'
 import BlogContainer from './components/blog-container/blog-container'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
 function App() {
 
   return (
     <div className="main-container min-h-screen flex flex-col w-full">
-    <NavBar/>
-    <div className="flex justify-center h-full w-full overflow-auto flex-grow">
-    <Router>
-      <Routes>
-        <Route path="/blog" element={<BlogListContainer />} />
-        <Route path="/blog/:slug" element={<BlogContainer />} />
-      </Routes>
-    </Router>
-        </div>
-    <Footer/>
+      <NavBar />
+      <div className="flex justify-center h-full w-full overflow-auto flex-grow">
+        <Router>
+          <Routes>
+            <Route path="/" element={<Navigate to="/blog" replace />} />
+            <Route path="/blog" element={<BlogListContainer />} />
+            <Route path="/blog/:slug" element={<BlogContainer />} />
+          </Routes>
+        </Router>
+      </div>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
